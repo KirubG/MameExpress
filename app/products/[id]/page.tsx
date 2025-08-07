b3463c8b194403b41d479717ba1ee1db3c5631a1
Kirubel Girma <Kirub1896@gmail.com>
2025-08-07 17:58:32 +0300

    fix issues with deployment

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,18 +1,25 @@
-import ProductDetail from "@/components/ProductDetail";
 import { stripe } from "@/lib/stripe";
+import ProductDetail from "@/components/ProductDetail";
 import React from "react";
 
-const productsPage = async ({ params }: { params: { id: string } }) => {
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+const ProductsPage = async ({ params }: PageProps) => {
   const product = await stripe.products.retrieve(params.id, {
     expand: ["default_price"],
   });
-        const plainProduct = JSON.parse(JSON.stringify(product));
+
+  const plainProduct = JSON.parse(JSON.stringify(product));
+
   return (
     <div>
-
-      <ProductDetail product={plainProduct}/> {/* because this page is server side component it fetch data from the server and pass it to the ProductDetail component */}
+      <ProductDetail product={plainProduct} />
     </div>
   );
 };
 
-export default productsPage;
+export default ProductsPage;
